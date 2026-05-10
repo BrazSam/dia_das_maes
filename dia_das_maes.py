6678afaef49534a78fc4f0efeb1103ce2019a9b5
Samuel Braz dos Santos <braz.samuel35@uniplaclages.edu.br>
2026-05-09 23:16:13 -0300

    primeira versao feita

--- dia_das_maes.py
+++ dia_das_maes.py
@@ -10,7 +10,7 @@ janela.title("Dia das Mães") # Título da janela
 titulo= tk.Label(
     janela, 
     text="Feliz Dia das Mães!", 
-    font=("Arial", 24), 
+    font=("Arial", 36, 'bold'), 
     bg="#ffd6e7", 
     fg="#ff69b4") # Criação de um rótulo com a mensagem de feliz dia das mães
 
@@ -19,7 +19,7 @@ titulo.pack(pady=50) # Posicionamento do rótulo na janela)
 texto = tk.Label(
     janela,
     text="Mãe, resgate seu presente 🎁",
-    font=("Arial", 16),
+    font=("Arial", 24, 'bold'),
     bg="#ffd6e7",
     fg="#ff69b4") # Criação de um rótulo com a mensagem para resgatar o presente
 
@@ -34,5 +34,15 @@ btn_presente = tk.Button(janela, text="Lavo a louça " \
                          "por uma semana", font=("Arial", 14), bg="#ff69b4", fg="white", command=resgatar_presente) # Criação de um botão para resgatar o presente
 
 btn_presente.pack(pady=20) # Posicionamento do botão na janela
+
+def mudar_texto(event):
+    btn_presente.config(text="Um abraço 😁")
+
+def voltar_texto(event):
+    btn_presente.config(text="Lavo a louça por 1 semana")
+
+btn_presente.bind("<Enter>", mudar_texto) # Evento para mudar o texto do botão quando o mouse passar por cima
+btn_presente.bind("<Leave>", voltar_texto) # Evento para voltar o texto do botão
+
 
 janela.mainloop()
