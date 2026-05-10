7ba839538569eccffd8989af4789f6443ecb9182
Samuel Braz dos Santos <braz.samuel35@uniplaclages.edu.br>
2026-05-09 23:07:38 -0300

    adicionando texto

---
+++ dia_das_maes.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import messagebox
+
+janela =tk.Tk() # Criação da janela principal
+janela.configure(bg = "#ffd6e7") # Configuração da cor de fundo da janela
+janela.geometry("600x400") # Configuração do tamanho da janela
+janela.title("Dia das Mães") # Título da janela
+
+
+titulo= tk.Label(
+    janela, 
+    text="Feliz Dia das Mães!", 
+    font=("Arial", 24), 
+    bg="#ffd6e7", 
+    fg="#ff69b4") # Criação de um rótulo com a mensagem de feliz dia das mães
+
+titulo.pack(pady=50) # Posicionamento do rótulo na janela)
+
+texto = tk.Label(
+    janela,
+    text="Mãe, resgate seu presente 🎁",
+    font=("Arial", 16),
+    bg="#ffd6e7",
+    fg="#ff69b4") # Criação de um rótulo com a mensagem para resgatar o presente
+
+texto.pack(pady=20) # Posicionamento do rótulo na janela
+
+
+janela.mainloop()
